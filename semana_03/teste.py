e46e6031aa0a24937a36cd03f7ecd6f00702f280
Valéria Cavalcanti <valeria.cavalcanti@ifpb.edu.br>
2021-02-22 20:58:32 -0300

    Semana 03

---
+++ semana_03/teste.py
@@ -0,0 +1,14 @@
+nome = input('Informe seu nome: ')
+idade = input('Informe sua idade:')
+print(nome)
+print(idade)
+#print(alem) - comentário
+print('até breve')
+print(type(nome))
+print(type(idade))
+
+idade = int(idade) # conversao de tipos
+
+print(idade, type(idade), idade, "fim")
+
+print(idade * 2)
