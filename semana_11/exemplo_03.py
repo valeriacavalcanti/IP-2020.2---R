8a0d173c4e4ea0134a78455ebfc3538c71842e75
Valéria Cavalcanti <valeria.cavalcanti@ifpb.edu.br>
2021-04-21 08:31:33 -0300

    Add semana_11/exemplo_03

---
+++ semana_11/exemplo_03.py
@@ -0,0 +1,11 @@
+instituicao = 'ifpb'
+
+for i in range(len(instituicao)):
+    print(instituicao[i], ord(instituicao[i]))
+
+print()
+
+instituicao = 'IFPB'
+
+for i in range(len(instituicao)):
+    print(instituicao[i], ord(instituicao[i]))
